6e153d6d03af6eddb2055871beba4775c41f2f4b
tyler stuart <t.stu21.11@gmail.com>
2018-07-14 10:37:47 -0700

    first attempt

---
+++ keys.js
@@ -0,0 +1,10 @@
+//Twitter Api key
+
+
+//Access Token	1017916183293509632-epxmjLpoypC5MNjXDlTryBkPI7HLBU
+//Access Token Secret	35yU2qAVW49DpRVp5iXl9tQsdk3ivYhDG5LtNFAUrYJGv
+//Access Level	Read and write
+//Owner	TylerSt24765168
+//Owner ID	1017916183293509632
+
+//havent received a spotify key in over 48 hours?
